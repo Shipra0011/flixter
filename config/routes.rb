cb22a6b2f46aa9cb2c962e3b1f964a07234a8016
Shipra0011 <surfriderfitness23@gmail.com>
2017-01-09 14:14:42 -0500

    devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index' 
   devise_for :users 
-    
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -57,4 +56,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+end 
