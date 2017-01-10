140c91a08fec32290eb39b91b37e7050c905c52a
Shipra0011 <surfriderfitness23@gmail.com>
2017-01-10 12:26:14 -0500

    Allow course creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#index' 
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
   devise_for :users 
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
