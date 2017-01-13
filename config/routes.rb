69fa73d4c6e2d1761fb3f6cc7973d5e0e457036d
Shipra0011 <surfriderfitness23@gmail.com>
2017-01-12 21:56:46 -0500

    Listing the courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users 
   root 'static_pages#index' 
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
-  devise_for :users 
+  
 
 
   # The priority is based upon order of creation: first created -> highest priority.
